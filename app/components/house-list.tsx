a16744b643999d023e790c160c36fbd3327006f9
Alex Panteli <alekspanteli@gmail.com>
2025-10-31 14:26:13 +0200

    Add state management to HouseList component for dynamic house addition; refactor houses array and update rendering logic.

--- app/components/house-list.tsx
+++ app/components/house-list.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import HouseRow from "./house-row";
 
 export type House = {
@@ -7,12 +10,25 @@ export type House = {
   price: number;
 };
 
-const houses: House[] = [
+const housesArray: House[] = [
   { id: 1, name: "House 1", address: "123 Main St", price: 100000 },
   { id: 2, name: "House 2", address: "456 Main St", price: 200000 },
 ];
 
 export default function HouseList() {
+  const [houses, setHouses] = useState<House[]>(housesArray);
+
+  const addHouse = () => {
+    setHouses([
+      ...houses,
+      {
+        id: 3,
+        name: "House 3",
+        address: "777 Main St",
+        price: 65600000,
+      },
+    ]);
+  };
   return (
     <>
       <h2>House List</h2>
@@ -25,11 +41,14 @@ export default function HouseList() {
           </tr>
         </thead>
         <tbody>
-          {houses.map((house) => (
-            <HouseRow key={house.id} {...house} />
+          {houses.map((houses) => (
+            <HouseRow key={houses.id} {...houses} />
           ))}
         </tbody>
       </table>
+      <button className="bg-blue-500 text-white p-2 rounded-md cursor-pointer" onClick={addHouse}>
+        Add House
+      </button>
     </>
   );
 }
